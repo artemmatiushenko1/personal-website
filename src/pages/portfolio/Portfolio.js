4a511d34b8fe18139a6564e773290dfe22c1686b
Artem Matiushenko <artom.matyushenko@gmail.com>
2022-03-10 16:26:43 +0200

    Added masonry images grid

--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -1,20 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { ref, listAll, getDownloadURL } from '@firebase/storage';
 import { storage } from '../../firebase/config';
 import * as S from './Portfolio.style';
 import Container from 'components/container';
+import { Spinner } from 'components/spinner';
+import Masonry from 'react-masonry-css';
+import { ThemeContext } from 'styled-components';
+
+const Photo = ({ src, alt }) => {
+  return (
+    <S.ImgContainer>
+      <img src={src} alt={alt} />
+    </S.ImgContainer>
+  );
+};
 
 const Portfolio = () => {
+  const { breakpoints } = useContext(ThemeContext);
   const [urls, setUrls] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    900: 2,
+    500: 1,
+  };
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const listRef = ref(storage, 'art');
       const res = await listAll(listRef);
       const paths = await Promise.all(
         res.items.map(async (item) => await getDownloadURL(item))
       );
       setUrls(paths);
+      setIsLoading(false);
       console.log(paths);
     };
 
@@ -24,9 +46,16 @@ const Portfolio = () => {
   return (
     <S.Section>
       <Container>
-        {urls.map((url) => {
-          return <img src={url} alt={'a'} width="200px" height="auto" />;
-        })}
+        <S.MasonryGrid
+          breakpointCols={breakpointColumnsObj}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column"
+        >
+          {urls.map((url) => {
+            return <Photo src={url} alt={url} />;
+          })}
+        </S.MasonryGrid>
+        {isLoading && <Spinner />}
       </Container>
     </S.Section>
   );
--- src/pages/portfolio/Portfolio.style.js
+++ src/pages/portfolio/Portfolio.style.js
@@ -1,7 +1,38 @@
 import styled from 'styled-components';
+import Masonry from 'react-masonry-css';
 
 export const Section = styled.section`
   padding: 120px 20px 0 20px;
   min-height: 100vh;
   background-color: #202020;
+`;
+
+export const MasonryGrid = styled(Masonry)`
+  &.my-masonry-grid {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    width: auto;
+  }
+
+  .my-masonry-grid_column {
+    background-clip: padding-box;
+  }
+
+  .my-masonry-grid_column:not(:first-child) {
+    padding-left: 20px;
+  }
+
+  .my-masonry-grid_column > div {
+    margin-bottom: 20px;
+  }
+`;
+
+export const ImgContainer = styled.div`
+  img {
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    display: block;
+  }
 `;
