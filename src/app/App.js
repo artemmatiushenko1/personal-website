f4029c60eacfae21eaf48bdabec148e2bbb465ef
Artem Matiushenko <artom.matyushenko@gmail.com>
2022-03-13 18:00:06 +0200

    Removed photos uploading feature

--- src/app/App.js
+++ src/app/App.js
@@ -8,7 +8,6 @@ import { useContext } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import Links from 'pages/links/Links.js';
 import Portfolio from 'pages/portfolio/Portfolio.js';
-import Upload from 'pages/upload/Upload.js';
 
 const App = () => {
   const { pathname } = useLocation();
@@ -27,7 +26,6 @@ const App = () => {
         <Route path="/nft" element={<Home />} />
         <Route path="/contacts" element={<Home />} />
         <Route path="/links" element={<Links />} />
-        <Route path="/upload" element={<Upload />} />
       </Routes>
     </S.App>
   );
--- src/redux/slices/artworksSlice.js
+++ src/redux/slices/artworksSlice.js
@@ -1,7 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { getDocs, collection, addDoc } from '@firebase/firestore';
-import { db, storage } from '../../firebase/config';
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { getDocs, collection } from '@firebase/firestore';
+import { db } from '../../firebase/config';
 
 export const getArtworks = createAsyncThunk(
   'artworks/getArtworks',
@@ -14,29 +13,6 @@ export const getArtworks = createAsyncThunk(
 
       console.log(artworks);
       return thunkApi.fulfillWithValue(artworks);
-    } catch (err) {
-      return thunkApi.rejectWithValue({ message: err.message });
-    }
-  }
-);
-
-export const postArtwork = createAsyncThunk(
-  'artworks/postArtwork',
-  async ({ name, categories, file, year }, thunkApi) => {
-    try {
-      const storageRef = ref(storage, `artworks/${file.name}`);
-      const snapshot = await uploadBytes(storageRef, file);
-      const downloadURL = await getDownloadURL(snapshot.ref);
-
-      const newArtwork = {
-        imgUrl: downloadURL,
-        createdAt: new Date().toISOString(),
-        name,
-        year,
-        categories,
-      };
-
-      await addDoc(collection(db, 'artworks'), newArtwork);
     } catch (err) {
       return thunkApi.rejectWithValue({ message: err.message });
     }
@@ -62,16 +38,6 @@ export const artworksSlice = createSlice({
       state.isLoading = false;
     },
     [getArtworks.rejected]: (state, { payload }) => {
-      state.isLoading = false;
-    },
-    [postArtwork.pending]: (state, { payload }) => {
-      state.error = null;
-      state.isLoading = true;
-    },
-    [postArtwork.fulfilled]: (state, { payload }) => {
-      state.isLoading = false;
-    },
-    [postArtwork.rejected]: (state, { payload }) => {
       state.isLoading = false;
     },
   },
